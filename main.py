14c781ca3158ecc142c6ffd6068a10a00ef385f7
ShaharMeiron <shaharmeiron@gmail.com>
2025-04-03 23:45:54 +0300

    started calculating directions

--- main.py
+++ main.py
@@ -1,38 +1,66 @@
 import cv2
 from ultralytics import YOLO
+import logging
 
 
 model = YOLO("yolov8n.pt")
+names = model.names
 
 cap = cv2.VideoCapture(0)
 
-while cap.isOpened():
-    success, frame = cap.read()
 
-    if success:
-        # Run YOLO tracking on the frame, persisting tracks between frames
-        results = model.track(frame, persist=True, )
-        print("**************************************************************************************************")
-        print(results)
-        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-        for box in results[0].boxes:
-            print(box.xyxy.tolist(), box.conf.item(), int(box.cls))  # Coordinates, confidence, class ID
+def get_first_results():
+    if cap.isOpened():
+        success, frame = cap.read()
 
-        quit()
-        # Visualize the results on the frame
-        annotated_frame = results[0].plot()
+        if success:
+            results = model.track(frame, persist=True, )
 
-        # Display the annotated frame
-        cv2.imshow("YOLO8 Tracking", annotated_frame)
+            for box in results[0].boxes:
+                print(f"object: {names[int(box.cls)]}, Confidence: {box.conf.item()}, Coordinates: {box.xyxy.tolist()}")
 
-        # Break the loop if 'q' is pressed
-        if cv2.waitKey(1) & 0xFF == ord("q"):
+            # Visualize the results on the frame
+            annotated_frame = results[0].plot()
+
+            cv2.imshow("YOLO8 Tracking", annotated_frame)
+            return results
+
+
+def main():
+    prev_results = get_first_results()
+    directions = {}
+    while cap.isOpened():
+        success, frame = cap.read()
+
+        if success:
+            results = model.track(frame, persist=True, )
+
+            # calculate direction - up or down
+            for box in results[0].boxes:
+                if box.cls == '0':  # if human
+                    current_box_id = box.id
+                    for prev_box in prev_results[0].boxes:
+                        if box.cls == '0' and current_box_id == prev_box.id:
+                            if current_box_id not in directions:
+                                directions[current_box_id] = box.xyxy.tolist()
+
+
+
+                # print(f"object: {names[int(box.cls)]}, Confidence: {box.conf.item()}, Coordinates: {box.xyxy.tolist()}")
+
+            # Visualize the results on the frame
+            annotated_frame = results[0].plot()
+
+            cv2.imshow("YOLO8 Tracking", annotated_frame)
+
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
+        else:
             break
-    else:
-        # Break the loop if the end of the video is reached
-        break
 
-# Release the video capture object and close the display window
-cap.release()
-cv2.destroyAllWindows()
+    cap.release()
+    cv2.destroyAllWindows()
 
+
+if __name__ == '__main__':
+    main()
